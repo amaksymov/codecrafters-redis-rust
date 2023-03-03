ad107ea28d25e3b80a8fabaa44217005b64bf482
Andrii <maximov.echo@gmail.com>
2023-03-03 15:44:54 +0200

    pass 2nd stage

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,37 @@
 // Uncomment this block to pass the first stage
-use std::net::TcpListener;
+use std::io::Write;
+use std::net::{TcpListener, TcpStream};
 
-fn main() {
+fn handle_client(mut stream: TcpStream) {
+    // let buf_reader = BufReader::new(&mut stream);
+    // let request: Vec<_> = buf_reader.lines()
+    //     .map(|result| result.unwrap())
+    //     .take_while(|line| !line.is_empty())
+    //     .collect();
+    // println!("{:?}", request);
+
+    let response = "+PONG\r\n";
+    stream.write_all(response.as_bytes()).unwrap();
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
     // Uncomment this block to pass the first stage
 
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:6379")?;
 
     for stream in listener.incoming() {
         match stream {
             Ok(_stream) => {
                 println!("accepted new connection");
+                handle_client(_stream);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+    Ok(())
 }
